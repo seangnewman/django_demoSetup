5e351e71946a70da0165cf3ab15467c3662a846e
seangnewman <seangnewman@msn.com>
2018-12-09 18:27:25 -0700

    created views

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,7 @@ from .models import Post
 # Register your models here.
 @admin.register(Post)
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish','status') 
     list_filter = ('status', 'created', 'publish', 'author')
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      # Customization
-     'blog.apps.BlogConfig',
+     'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
