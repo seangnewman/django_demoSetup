13ade974adc26ffc2f7465e3aaf81046e7e9c62e
seangnewman <seangnewman@msn.com>
2018-12-09 18:47:43 -0700

    resolved issue with detail view

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, 
                              slug = post,
                              status = 'published',
-                             publish_year = year, 
-                             publish_month = month, 
-                             publish_day = day)
+                             publish__year = year, 
+                             publish__month = month, 
+                             publish__day = day)
     return render(request, 'blog/post/detail.html', {'post':post})
